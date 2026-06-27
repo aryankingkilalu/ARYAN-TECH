357fa1235a75ce44ef32a4e31f529876bf94e0a1
aryankingkilalu <aryankingkilalu@gmail.com>
2026-06-27 22:33:11 +0300

    Aryan

---
+++ settings.js
@@ -0,0 +1,34 @@
+///Copyright © Aryan x 
+const settings = {
+packname: process.env.PACK_NAME || 'aryan',
+  
+author: process.env.AUTHOR || 'aryan',
+  
+SESSION_ID: process.env.SESSION_ID || '', ///PASTE YOUR SESSION ID HERE "Aryan-X:~"
+  
+botName: process.env.BOT_NAME || "∆RY∆N-X",
+  
+botOwner: process.env.BOT_OWNER || 'ARYAN',
+
+Botprefix: process.env.PREFIX || '.',
+  
+ownerNumber: process.env.OWNER_NUMBER || '255772336513',
+  
+giphyApiKey: process.env.GIPHY_API_KEY || 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  
+commandMode: process.env.COMMAND_MODE || "public",
+  
+maxStoreMessages: parseInt(process.env.MAX_STORE_MESSAGES) || 20,
+  
+storeWriteInterval: parseInt(process.env.STORE_WRITE_INTERVAL) || 10000,
+  
+description: process.env.BOT_DESCRIPTION || "This is a bot for managing group commands and automating tasks.",
+  
+version: process.env.BOT_VERSION || "2.0.0",
+  
+updateZipUrl: process.env.UPDATE_ZIP_URL || "https://github.com/Teddytech1/TEDDY-XMD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
+
+///Copyright © Aryan x 
